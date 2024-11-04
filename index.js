38a8a9d681bdf9b037a4721e78fcd7001cf467a5
Alex <78886791+alexander2475@users.noreply.github.com>
2024-11-04 14:27:52 -0700

    Updating index.js

--- index.js
+++ index.js
@@ -1,43 +1,5 @@
-function titleAnimation(elementId, speed, callback) {
-  let target = document.getElementById(elementId);
-
-  if (!target) {
-    console.error("Element not found: ", elementId);
-    return;
-  }
-
-  target.style.display = "block";
-
-  let targetText = target.textContent;
-  target.textContent = "";
-
-  let charIndex = 0;
-
-  let charInterval = setInterval(function () {
-    if (charIndex < targetText.length) {
-      target.textContent += targetText[charIndex++];
-    } else {
-      clearInterval(charInterval);
-      if (callback) {
-        callback();
-      }
-    }
-  }, speed);
-}
-
 function themeToggle() {
   document.documentElement.classList.toggle("dark");
   document.body.classList.toggle("dark");
 }
 
-// document.addEventListener("DOMContentLoaded", function () {
-//   document.body.style.overflowX = "hidden";
-//
-//   titleAnimation('headerTemp', 100, function() {
-//     titleAnimation('headerTitle', 100, function() {
-//       titleAnimation('headerBody', 50, function() {
-//           document.body.style.overflowY = "auto";
-//       });
-//     });
-//   });
-// });
